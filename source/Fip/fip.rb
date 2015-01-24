ca2d58273dde23d0cabdca9bb0a83dad317a30ad
m-nakada <m-nakada@nekojarashi.com>
2015-01-24 23:13:59 +0900

    Fix bugs

--- source/Fip/fip.rb
+++ source/Fip/fip.rb
@@ -1,20 +1,73 @@
 # encoding: utf-8
-# require 'fileutils'
+require 'etc'
+require 'fileutils'
+require 'syslog'
 
-input=ENV['POPCLIP_TEXT']
+FIP_DIR = '/Dropbox/fip'
+Syslog.open("fip.rb")
+input = ENV['POPCLIP_TEXT']
 
-# Popclipからで呼びだした場合、File.expand_path()を実行すると、スクリプトが失敗する
-# dir = File.expand_path("~/Dropbox/fip/")
-# unless File.directory?(dir)
-#   FileUtils.mkdir_p(dir)
-# end
+# input = <<"EOS"
 
-path = "/Users/m-nakata/Dropbox/fip/" + Time.new.strftime("%Y-%m-%d").to_s + ".txt"
-File.open(path, 'a') {|f|
-  f << "```\n"
-  f << input + "\n"
-  f << "```\n"
-  f << "\n"
-}
+# Pierre Vassiliu
+# Qui C Est Celui La Album : Master Series (Compilation) (1974)
+# EOS
 
-print input
+class Exporter
+  def initialize(input, dir)
+    @input = input
+    @dir = dir
+  end
+  attr_accessor :input, :dir, :content
+  
+  def prepare_directory
+    unless File.directory?(@dir)
+      FileUtils.mkdir_p(@dir)
+    end
+  end
+
+  def prepare_content
+    array = []
+    @input.each_line do |line|
+      next if line == "\n"
+      pos = line.rindex(' Album : ')
+      if pos then
+        line.insert(pos, "\n") 
+        line.each_line {|subline| array.push subline.strip}
+      else
+        array.push line.strip
+      end
+    end
+    @content = array
+  end
+
+  def export_content
+    path = dir + '/' + Time.new.strftime("%Y-%m-%d").to_s + ".txt"
+    File.open(path, 'a+') { |f|
+      f << "```\n"
+      @content.each {|c| f << c + "\n"}
+      f << "```\n"
+      f << "\n"
+    }
+  end
+
+  def print_content
+    @content.each {|c| puts c}
+  end
+  
+  def export
+    prepare_directory
+    prepare_content
+    export_content
+    # print_content
+  end
+end
+
+begin
+  exporter = Exporter.new(input, "#{Etc.getpwuid.dir}" + FIP_DIR)
+  exporter.export
+rescue Exception => e
+  Syslog.log(Syslog::LOG_WARNING, "Exception: %s", e.message)
+ensure
+  Syslog.close
+end
