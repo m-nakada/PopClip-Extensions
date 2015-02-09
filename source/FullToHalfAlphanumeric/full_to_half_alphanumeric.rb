fc28a213b3fdfdf42498e111648c9c388736fcb2
m-nakada <m-nakada@nekojarashi.com>
2015-02-09 11:40:21 +0900

    Add FullToHalfAlphanumeric

---
+++ source/FullToHalfAlphanumeric/full_to_half_alphanumeric.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+require 'nkf'
+require 'syslog'
+
+input = ENV['POPCLIP_TEXT']
+
+# Test
+# input = <<TEST
+# １２３４５６７８９０
+# ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ
+# ，．：；？！´｀＾＿—／＼｜‘’“”（）［］｛｝〈〉＋−＝＜＞＄％＃＆＊＠
+# あいうえお
+# アイウエオ 
+# TEST
+
+begin
+  Syslog.open("PopClip:FullToHalfAlphanumeric")
+  print NKF::nkf '--utf8 -Z0', str
+rescue Exception => e
+  Syslog.warning("Exception: %s", e.message)
+ensure
+  Syslog.close
+end
